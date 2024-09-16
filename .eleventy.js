15f9e0d958f8d4fdd79e6ec91ee1475c28d3c213
Nathan Arvan <nathannarvan@gmail.com>
2024-09-16 14:41:09 -0500

    add syntax highlighting and code css styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const { DateTime } = require('luxon');
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
+    
+    eleventyConfig.addPlugin(syntaxHighlight);
 
     eleventyConfig.addFilter('readableDate', (dateObj) => {
       return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
