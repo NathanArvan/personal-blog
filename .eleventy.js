ebb19bdf23b508e7d7693824b440fa96bbda5158
Nathan Arvan <nathannarvan@gmail.com>
2024-10-25 14:13:56 -0500

    add eleventy image package

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const { DateTime } = require('luxon');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
@@ -10,5 +11,25 @@ module.exports = function(eleventyConfig) {
       return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
         'dd LLL yyyy'
       );
+    })
+
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+      // which file extensions to process
+      extensions: "html",
+
+      // Add any other Image utility options here:
+  
+      // optional, output image formats
+      formats: ["webp", "jpeg"],
+      // formats: ["auto"],
+  
+      // optional, output image widths
+      // widths: ["auto"],
+  
+      // optional, attributes assigned on <img> override these values.
+      defaultAttributes: {
+        loading: "lazy",
+        decoding: "async",
+      },
     })
   };
